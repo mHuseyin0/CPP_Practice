5a4eab688d967b986618ca00c88afcd9617a37f5
mHuseyin0 <muhammedhaydin@gmail.com>
2024-07-19 17:16:23 +0300

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if [[ neofetch == *"Android"* ]]; then
-    os="Android"
-else
-    os="Linux"
-fi
+function compileCode() {
+  fastOut=$(fastfetch)
 
-g++ main.cpp DVDStoreManagementSystem.cpp -Wall -O2 -ansi -o "$os" &&
-    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-origins=yes --log-file=valgrind-out.txt ./$os
+  if [[ $fastOut == *"Android"* ]]; then
+    os="Android"
+  else
+    os="Linux"
+  fi
+
+  echo "Compiling for $os."
+  g++ *.cpp -Wall -O -ansi -o "$os" &&
+    if [[ $os == "Android" ]]; then
+      echo "Valgrind will not be executed since you are on android. Finishing."
+      return 0
+    fi
+
+  echo "Valgrind is running. Please wait."
+  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-origins=yes --log-file=valgrind-out.txt ./$os
+
+  echo "Valgrind output saved in valgrind-out.txt. Finishing."
+  return 0
+}
+compileCode
